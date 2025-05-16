82e0b26b6ca885c09cc2cf18e02f715a55e3f12d
Souhila Yousfi <souhila.yousfi@vif.fr>
2025-05-16 23:07:26 +0200

    Code improvements

--- src/Service/InvoiceParser.php
+++ src/Service/InvoiceParser.php
@@ -5,64 +5,83 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Entity\Invoice;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Exception\InvalidArgumentException;
 
 
 class InvoiceParser
 {
-    private EntityManagerInterface $em;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $this->em = $em;
+        $this->entityManager = $entityManager;
     }
 
-    public function parse(string $fp): void
+    /*
+     * Parse and update database from the input filePath
+     */
+    public function parse(string $filePath): void
     {
-        if (str_contains($fp, 'json')) {        //Pour les json
-            $f = file_get_contents($fp);
-            $d = preg_split("/\r\n|\n|\r/", $f);
-            $c = 0;
-            $m = "";
-            $n = "";
-            /** Tant qu'il y a une ligne */
-            while(true){
-                if(isset($d[$c])){
-                    if(str_contains($d[$c], "montant")){
-                    $m = explode(": ", $d[$c])[1];
-                    $m = substr($m, 0, strlen($m) - 1);
-                    }
-                    if(str_contains($d[$c], "nom")){
-                    $n = explode(": ", $d[$c])[1];
-                    $n = substr($n, 0, strlen($n) - 1);
-                    }
-                    if(str_contains($d[$c], "}")){
-                        $this->em->getConnection()->executeStatement(
-                "UPDATE invoice SET amount = {$m} WHERE name = '{$n}'"
-                    );
-                    }
-                    $c++;
-                }else{
-                break;
-                }
-            }
-        } elseif (str_contains($fp, 'csv')) {   //Pour les json
-
-
-            $d = array_map(function($r) {
-                return str_getcsv($r, "\t");
-            }, file($fp));
-            $c = 0;
-                while(true){
-                if(isset($d[$c])){
-                    $this->em->getConnection()->executeStatement(
-                        "UPDATE invoice SET amount = {$d[$c][0]} WHERE name = '{$d[$c][2]}'"
-                    );
-                    $c++;
-                }else{
-                    break;
-                }
-                }
+        if (!file_exists($filePath)) {
+            throw new InvalidArgumentException("File not found: {$filePath}");
         }
+        if(str_contains($filePath, "json")) {
+           $this->parseJsonFile($filePath);
+        }elseif (str_contains($filePath, "csv")) {
+            $this->parseCsvFile($filePath);
+        }else {
+            throw new InvalidArgumentException("Extension not supported: {$filePath}");
+        }
+    }
+
+    private function parseJsonFile(string $filePath): void
+    {
+        $content = file_get_contents($filePath);
+        $lines = preg_split("/\r\n|\n|\r/", $content);
+        $amount="";
+        $name="";
+        foreach ($lines as $line) {
+            if(str_contains($line, "montant")) {
+                $amount = $this->extractValue($line);
+            } elseif(str_contains($line, "nom")) {
+                $name = $this->extractValue($line);
+            } elseif(str_contains($line, "}")) {
+                $this->updateInvoice($amount, $name);
+            }
+        }
+    }
+
+
+    private function parseCsvFile(string $filePath): void
+    {
+        $rows = array_map(function ($row) {
+            return str_getcsv($row, "\t");
+        }, file($filePath));
+        foreach ($rows as $row) {
+            if (count($row)>=3) {
+                $amount = $row[0];
+                $name = $row[2];
+                $this->updateInvoice($amount, $name);
+            }
+        }
+    }
+
+    private function updateInvoice(string $amount, string $name): void{
+        $sql = 'UPDATE invoice SET amount = :amount WHERE name = :name';
+        $this->entityManager->getConnection()->prepare($sql)->executeStatement([
+            'amount' => $amount,
+            'name' => $name
+        ]);
+    }
+
+    private function extractValue(string $line): string{
+
+        $parts = explode(": ", $line);
+        // ["montant", "852.38,"]
+        // ["nom", "Frank Green,"]
+        $value = trim($parts[1]);
+        return $value;
     }
 }
